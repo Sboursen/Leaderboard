d70aa78575e058ccbc0d8d851fe801565ae67bd4
sboursen <dev.sboursen@gmail.com>
2022-02-28 19:52:44 +0100

    Install babel with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,16 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ],
   },
 };
