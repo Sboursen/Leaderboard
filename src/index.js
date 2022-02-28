74b0209202c0e43c0e6aa93ef3b4337ce2a0e65a
sboursen <dev.sboursen@gmail.com>
2022-02-28 21:42:36 +0100

    Add Stylelint config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
 
-const div = `<h1 class='text-center text-2xl text-red-700'>hello</h1>`;
+const div = '<h1 class=\'text-center text-2xl text-red-700\'>hello</h1>';
 
 document.body.innerHTML += div;
