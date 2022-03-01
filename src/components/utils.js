b0e3f2f6dfd561e490efa7f944e8b3b2ab3e0e63
sboursen <dev.sboursen@gmail.com>
2022-03-01 12:29:40 +0100

    Add dom elements in utils

---
+++ src/components/utils.js
@@ -0,0 +1,11 @@
+export const scoreList = document.querySelector('.list');
+export const nameInput =
+  document.querySelector('#name-input');
+export const scoreInput =
+  document.querySelector('#score-input');
+export const submitButton = document.querySelector(
+  '.submit-button',
+);
+export const refreshButton = document.querySelector(
+  '.refresh-button',
+);
