34ab36ebf2c8d1d9192d222a39a70f4cb161b8ba
sboursen <dev.sboursen@gmail.com>
2022-03-01 12:56:33 +0100

    Implement displayScores methode

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,31 @@
 import leaderBoard from './api';
-import score from './score';
+import Score from './score';
+import { scoreList } from './utils';
 export default class Application {
   constructor() {
     this.leaderBoard = leaderBoard;
+    this.scoreList = scoreList;
   }
 
-  displayScores = () => {};
+  #getScoreFromAPI = (score) => {
+    const scoreObj = new Score(score.name, score.score);
+    return scoreObj;
+  };
 
-  #createScoreElement = (score) => `<li>${score.getName}: ${score.getScore}</li>`;
+  #createScoreElement = (score) =>
+    `<li>${score.getName()}: ${score.getScore()}</li>`;
+
+  #clearList = () => {
+    this.scoreList.innerHTML = '';
+  };
+
+  displayScores = () => {
+    this.#clearList();
+    let allScores = '';
+    this.leaderBoard.forEach((s) => {
+      const score = this.#getScoreFromAPI(s);
+      allScores += this.#createScoreElement(score);
+    });
+    this.scoreList.innerHTML = allScores;
+  };
 }
