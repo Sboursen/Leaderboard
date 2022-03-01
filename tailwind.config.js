9d89c110e766777efe22ba45579913e239a5ea22
sboursen <dev.sboursen@gmail.com>
2022-03-01 10:38:54 +0100

    Add basic page style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['"Comic Neue"', 'cursive'],
+      },
+    },
   },
   plugins: [],
 };
