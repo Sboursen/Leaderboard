b897b294a429d469c68c6dd3036f0f80b561fe17
sboursen <dev.sboursen@gmail.com>
2022-03-03 16:09:18 +0100

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"Comic Neue"', 'cursive'],
+        body: ['"Arima Madurai"', 'cursive'],
+        title: ['Parisienne', 'cursive'],
+      },
+      colors: {
+        rajeh: '#ffae63',
+        bittersweet: '#ff6d6c',
       },
     },
   },
