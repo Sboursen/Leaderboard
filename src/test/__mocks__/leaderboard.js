6df51e2e5d0c22f801706261e708eb0a3a110251
sboursen <dev.sboursen@gmail.com>
2022-03-03 11:29:36 +0100

    Implement SortArrByProperty utility function

--- src/test/__mocks__/leaderboard.js
+++ src/test/__mocks__/leaderboard.js
@@ -1,13 +1,13 @@
 const leaderBoard = [
-  { name: 'Name', score: '100' },
-  { name: 'Name', score: '200' },
-  { name: 'Name', score: '300' },
-  { name: 'Name', score: '400' },
-  { name: 'Name', score: '500' },
-  { name: 'Name', score: '600' },
-  { name: 'Name', score: '700' },
-  { name: 'Name', score: '800' },
-  { name: 'Name', score: '900' },
+  { user: 'Name2', score: '100' },
+  { user: 'Name3', score: '200' },
+  { user: 'Name9', score: '300' },
+  { user: 'Name8', score: '400' },
+  { user: 'Name4', score: '500' },
+  { user: 'Name5', score: '600' },
+  { user: 'Name7', score: '700' },
+  { user: 'Name6', score: '800' },
+  { user: 'Name1', score: '900' },
 ];
 
 export default leaderBoard;
--- src/test/tests/sortArrayByProperty.test.js
+++ src/test/tests/sortArrayByProperty.test.js
@@ -2,12 +2,34 @@ import { sortArrayByProperty } from '../../components/utils';
 import leaderBoard from '../__mocks__/leaderboard';
 
 describe('sortArrayByProperty', () => {
-  it('sorts a scores array', () => {
+  it('sorts a small scores array', () => {
     const scoresArr = leaderBoard.slice(0, 3);
     const expectedSortedArr = [
-      { user: 'Name5', score: '300' },
-      { user: 'Name4', score: '200' },
-      { user: 'Name1', score: '100' },
+      { user: 'Name9', score: '300' },
+      { user: 'Name3', score: '200' },
+      { user: 'Name2', score: '100' },
+    ];
+
+    const sortedScoresArr = sortArrayByProperty(
+      scoresArr,
+      'score',
+    );
+
+    expect(sortedScoresArr).toEqual(expectedSortedArr);
+  });
+
+  it('sorts a larger scores array', () => {
+    const scoresArr = leaderBoard;
+    const expectedSortedArr = [
+      { user: 'Name1', score: '900' },
+      { user: 'Name6', score: '800' },
+      { user: 'Name7', score: '700' },
+      { user: 'Name5', score: '600' },
+      { user: 'Name4', score: '500' },
+      { user: 'Name8', score: '400' },
+      { user: 'Name9', score: '300' },
+      { user: 'Name3', score: '200' },
+      { user: 'Name2', score: '100' },
     ];
 
     const sortedScoresArr = sortArrayByProperty(
